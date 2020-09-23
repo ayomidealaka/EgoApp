856061b2c687d5846c0c96e16ed978e1456da220
ayomideAlaka <muizzyomide@gmail.com>
2020-09-23 23:36:50 +0100

    Initial commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import logo from './../../asset/logo.png';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <img src={logo} alt="Ego" />;
+    </nav>
+  );
+};
+
+export default Navbar;
