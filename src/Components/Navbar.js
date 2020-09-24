b2458f775fa8db30edcd6a5cabae6f6643b1416a
ayomideAlaka <muizzyomide@gmail.com>
2020-09-24 19:50:33 +0100

    Added nav links.

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,33 @@
 import React from 'react';
 
-import logo from './../../asset/logo.png';
+import logo from './../asset/logo.png';
+import style from './../App.css';
+import { NavLink, Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav>
-      <img src={logo} alt="Ego" />;
+      <div className={style.navigation}>
+        <Link to="/" className={style.LogoLink}>
+          <img src={logo} alt="Ego" />
+          {/* <h1>Égo</h1> */}
+        </Link>
+        <Link to="/about" className={style.Link}>
+          About
+        </Link>
+        <Link to="/contact" className={style.Link}>
+          Contact
+        </Link>
+        <Link to="/plans" className={style.Link}>
+          Plans
+        </Link>
+      </div>
+      <Link to="/signup" className={style.Link_signup}>
+        <div className={style.Signup_Button}>Sign Up</div>
+        <div className={style.overlay}>
+          <div className={style.arrow}>Sign Up</div>
+        </div>
+      </Link>
     </nav>
   );
 };
