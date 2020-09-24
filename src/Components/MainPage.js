968abb091cdd6520381b556c0240ff5306e41e94
ayomideAlaka <muizzyomide@gmail.com>
2020-09-24 19:50:14 +0100

    Added Body component.

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
+import './../App.css';
+import Navbar from './Navbar';
+
+import style from './../App.css';
+import Body from './Body';
+
 const MainPage = () => {
-  return <div className="MainPage"></div>;
+  return (
+    <div className={style.Mainpage}>
+      <Navbar />
+      <Body />
+    </div>
+  );
 };
 
 export default MainPage;
