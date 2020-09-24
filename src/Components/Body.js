6fa2f097253ac868bb5c778cf65bd289294a7a25
ayomideAlaka <muizzyomide@gmail.com>
2020-09-24 19:47:57 +0100

    Added body component.

---
+++ src/Components/Body.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import logo from './../asset/face1.PNG';
+import style from './../App.css';
+import { NavLink, Link } from 'react-router-dom';
+
+const Body = () => {
+  return (
+    <div className={style.Body_text}>
+      <h1>
+        Immerse mobile banking with{' '}
+        <div className={style.ms_slider}>
+          <ul className={style.ms_slider_words}>
+            <li className={style.ms_slider_word}>culture</li>
+            <li className={style.ms_slider_word}>swag</li>
+            <li className={style.ms_slider_word}>dexterity</li>
+            <li className={style.ms_slider_word}>culture</li>
+          </ul>
+        </div>
+      </h1>
+      <h3>
+        Handle a business of any size - from startup to large enterprise. Use
+        spikes's software or other APIs to accept payments.
+      </h3>
+      <span>
+        <Link to="/signup" className={style.getStarted}>
+          <div className={style.getStarted_Button}>Get started</div>
+        </Link>
+        <p className={style.itsFree}>It's free</p>
+      </span>
+    </div>
+  );
+};
+
+export default Body;
